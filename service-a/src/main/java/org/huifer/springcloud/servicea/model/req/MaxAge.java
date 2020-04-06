83b9e7efdb357c2f7da423d41c20b58c27dff6b0
huifer <huifer97@163.com>
2020-04-06 21:53:20 +0800

    回滚

--- service-a/src/main/java/org/huifer/springcloud/servicea/model/req/MaxAge.java
+++
@@ -1,7 +0,0 @@
-package org.huifer.springcloud.servicea.model.req;
-
-public interface MaxAge {
-
-  long i = 1L;
-
-}
--- service-b/src/main/java/org/huifer/springcloud/serviceb/controller/TestController.java
+++ service-b/src/main/java/org/huifer/springcloud/serviceb/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,6 +18,10 @@ public class TestController {
 
     @Autowired
     HttpServletRequest httpServletRequest;
+
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/app-one")
     public String appOne() {
